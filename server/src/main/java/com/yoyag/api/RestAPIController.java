f54e06e99e67611db5b5ca73f34489c71a2e1ffa
Ben Surkiss <bensur8@gmail.com>
2018-05-16 19:41:20 +0300

    Comment out IPs handling

--- server/src/main/java/com/yoyag/api/RestAPIController.java
+++ server/src/main/java/com/yoyag/api/RestAPIController.java
@@ -36,24 +36,24 @@ public class RestAPIController {
     
 	@RequestMapping(value = "/post", method = RequestMethod.POST)
     public ResponseEntity<String> post(@RequestBody NewInput input, HttpServletRequest request) throws ServletException {
-		List<String> ips = new ArrayList<String>();
-		try {
-			ips.add(request.getRemoteAddr());
-			ips.add(request.getHeader("X-FORWARDED-FOR"));
-		} catch (Exception e) {
-			LOGGER.error("Got error while adding ips");
-			LOGGER.error(e);
-		}
-		try {
-			Object data = input.getData().get("data");
-			Map<String, Object> mapData;
-			mapData = (Map<String, Object>)data;
-			mapData.put("ips", ips);
-			LOGGER.info("IPs=" + ips);
-		} catch (Exception e) {
-			LOGGER.error("Could not add ips to input object");
-			LOGGER.error(e);
-		}
+//		List<String> ips = new ArrayList<String>();
+//		try {
+//			ips.add(request.getRemoteAddr());
+//			ips.add(request.getHeader("X-FORWARDED-FOR"));
+//		} catch (Exception e) {
+//			LOGGER.error("Got error while adding ips");
+//			LOGGER.error(e);
+//		}
+//		try {
+//			Object data = input.getData().get("data");
+//			Map<String, Object> mapData;
+//			mapData = (Map<String, Object>)data;
+//			mapData.put("ips", ips);
+//			LOGGER.info("IPs=" + ips);
+//		} catch (Exception e) {
+//			LOGGER.error("Could not add ips to input object");
+//			LOGGER.error(e);
+//		}
 		Parser p = getParserForInput(input);
 		p.parseInput(input);
 		return new ResponseEntity<String>(HttpStatus.NO_CONTENT);
